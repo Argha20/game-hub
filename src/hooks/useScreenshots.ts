2795948a3f28cdf9d2ab50a0395987e9685d35b5
Arghadeep <arghadeep2000m@gmail.com>
2024-05-01 01:03:50 +0530

    Build game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import { ScreenShot } from "../entities/Screenshot";
+
+ 
+const useScreenshot = (gameId: number) =>{
+   const apiClient = new APIClient<ScreenShot>(`/games/${gameId}/screenshots`);
+
+   return useQuery({
+       queryKey: ['Screenshot', gameId],
+       queryFn: apiClient.getAll
+    });
+}; 
+
+ export default useScreenshot;
